bb514f38e342341f448d936bcc9c5ad9cf018de5
leandjb <leandjb@gmail.com>
2023-02-06 14:31:56 -0500

    add new JAR lib

--- src/main/java/run/main.java
+++ src/main/java/run/main.java
@@ -1,14 +1,36 @@
 package run;
 
-import sdkExamples.X9MakeX9;
+import sdkExamples.X9RuntimeLicenseKey;
+
 
 public class main {
 
     public static void main(String[] args) {
+        /*
 
-        X9MakeX9 x9MakeX9 = new X9MakeX9;
+         * Set the runtime license key (this class is part of sdk-examples). X9RuntimeLicenseKey
 
+         * must be updated with the license key text provided for your evaluation.
 
+         */
+
+        X9RuntimeLicenseKey.setLicenseKey();
+
+        /*
+
+         * Initialize the environment.
+
+         */
+
+        X9SdkRoot.logStartupEnvironment(X9REFORMATX9);
+
+        X9SdkRoot.loadXmlConfigurationFiles();
+
+        if (!sdkBase.bindConfiguration(X9.X9_37_CONFIG)) {
+
+            throw X9Exception.abort("bind unsuccessful");
+
+        }
 
     }
 }
